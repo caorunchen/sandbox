d1acdd6744045bd3029291843fb036e1b1e86329
Runchen.CAO <caorunchen@hotmail.com>
2019-01-26 23:57:04 +0800

    for test

--- sand1.py
+++ sand1.py
@@ -1,1 +1,13 @@
-print("this is github,for test")
+fo = open("channel_brkt_modal.fem")
+ls = []
+for line in fo:
+    if "CQUAD4" in line[:6]:
+        line_array=(line.split(" "))
+        for i in range(line_array.count("")):
+            line_array.remove("")
+        ls.append(list(map(eval,line_array[1:3])))
+cbeam_element_id = tuple(ls)
+for i in range(len(cbeam_element_id)):
+    if cbeam_element_id[i][0] == 37:
+        print(cbeam_element_id[i][1])
+
---
+++ sandbox2_remove.py
@@ -0,0 +1,4 @@
+ls = "asdf asdfa sdfadf"
+lns = ls.replace(" ","")
+print(ls)
+print(lns)
